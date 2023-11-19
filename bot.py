4aa9c8ae8ff7a0c6cec871879f1007d647d1b5f5
gleb <webrushgithub@gmail.com>
2023-11-19 20:26:51 +0600

    Запустил бота

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+    bot.reply_to(message, "Привет! Я бот для управления чатом 😌")
 
 
 @bot.message_handler(commands=['ban'])
@@ -18,15 +18,25 @@ def ban_user(message):
         user_status = bot.get_chat_member(chat_id, user_id).status
         # проверка пользователя
         if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
+            bot.reply_to(message, "Невозможно забанить администратора 👨‍💻")
         else:
             # пользователь с user_id будет забанен в чате с chat_id
             bot.ban_chat_member(chat_id, user_id)
             bot.reply_to(
-                message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+                message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен 🥰")
     else:
         bot.reply_to(
-            message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+            message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить 🧐")
+
+
+@bot.message_handler(func=lambda message: "бот" and "плохой" in message.text.lower())
+def handle_message(message):
+    try:
+        bot.ban_chat_member(message.chat.id, message.from_user.id)
+        bot.reply_to(
+            message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен 🥰")
+    except:
+        bot.reply_to(message, "Забанил бы тебя, но ты админ 😡")
 
 
 bot.infinity_polling(none_stop=True)
